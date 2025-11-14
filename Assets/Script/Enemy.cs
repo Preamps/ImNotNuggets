78e10afa6565d83445c16f28511d5f414d567477
Preamps <Preampstv@gmail.com>
2025-11-14 16:37:31 +0700

    enemy move

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Enemy : Character
+{
+    private int damageHit;
+    public Transform player;
+    public float speed = 2f;
+    public int DamageHit
+    {
+        get
+        {
+            return damageHit;
+        }
+        set
+        {
+            damageHit = value;
+        }
+    }
+    private void Update()
+    {
+        Enemymove();
+    }
+
+    public void Enemymove() // enemy เดินหาผู้เล่น
+    {
+        if (player == null) return;
+
+        Vector2 direction = (player.position - transform.position).normalized;
+        
+        transform.position = (Vector2)transform.position +
+                            direction * speed * Time.deltaTime;
+
+    }
+
+}
